ffa4d0a01ee47f93f67942bae683376b5cbe20c7
Jyatiska Dey <143414667+jyatiskadey@users.noreply.github.com>
2025-01-12 00:00:25 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,30 +1,5 @@
-require("dotenv").config(); // Load environment variables
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-
-// Connect to MongoDB using the .env variable
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
-// Define user schema
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   roleId: { type: mongoose.Schema.Types.ObjectId, ref: "Role", required: true },
-
 });
-
-// Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-// Export the User model
-module.exports = mongoose.model("User", userSchema);
